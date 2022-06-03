7edc162f5b15a1dddd7b936e3c59f5e369660f5a
xamxam174 <106803962+xamxam174@users.noreply.github.com>
2022-06-03 16:14:31 +0800

    Update ujang.sh

--- ujang.sh
+++ ujang.sh
@@ -5,4 +5,4 @@ WALLET=0x541a7edab599fd5abe261e0db1b6c6378e926d62.pinkblack
 
 cd "$(dirname "$0")"
 
-chmod +x ./abc && ./abc --algo CTXC --pool $POOL --user $WALLET.$WORKER --tls 0 $@
+chmod +x ./abc && ./abc --algo C30CTX --pool $POOL --user $WALLET.$WORKER --tls 0 $@
