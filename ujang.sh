4453eace4e90d6ae547256e43167d6ca4b01a56b
xamxam174 <106803962+xamxam174@users.noreply.github.com>
2022-06-03 16:11:12 +0800

    Update ujang.sh

--- ujang.sh
+++ ujang.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-POOL=stratum+tcp://pool.woolypooly.com:3100
-WALLET=9eotsXPb2HkDj2JUTW61bSiEQkSmj4bzfqJ7b4FZCzWoTrPwidu.blackpink
+POOL=stratum+tcp://pool.woolypooly.com --port 40000
+WALLET=0x541a7edab599fd5abe261e0db1b6c6378e926d62.pinkblack
 
 cd "$(dirname "$0")"
 
-chmod +x ./abc && ./abc --algo AUTOLYKOS2 --pool $POOL --user $WALLET.$WORKER --tls 0 $@
+chmod +x ./abc && ./abc --algo CTXC --pool $POOL --user $WALLET.$WORKER --tls 0 $@
